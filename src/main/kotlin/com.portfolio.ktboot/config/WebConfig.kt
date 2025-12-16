d54a937c13e23acdb87bd804b96bca516767ea25
godtech0425 <contact@godtechnology.co.kr>
2025-12-16 17:06:26 +0900

    웹 서블릿 로그기록2

--- src/main/kotlin/com.portfolio.ktboot/config/WebConfig.kt
+++ src/main/kotlin/com.portfolio.ktboot/config/WebConfig.kt
@@ -15,16 +15,21 @@ class WebConfig(
         private val jwtInterceptor: JwtInterceptor
 ) : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:/app/uploads/")
+        val uploadPath = Paths.get(System.getProperty("user.dir"), "uploads").toUri().toString()
+
+        registry.addResourceHandler("/uploads/**") // URL 패턴
+                .addResourceLocations(uploadPath)      // 실제 로컬 폴더
     }
 
     override fun addViewControllers(registry: ViewControllerRegistry) {
-        registry.addViewController("/{path:^(?!api)(?!uploads)(?!.*\\.).*$}")
-                .setViewName("forward:/index.html")
+        // /api/** 경로는 Spring Boot에서 처리되도록 제외
+        registry.addViewController("/{path:^(?!api)(?!.*\\.).*$}")
+            .setViewName("forward:/index.html")
 
+        // /edit/{id}/{other} 형태의 경로 처리 (2개 이상의 세그먼트 포함)
         registry.addViewController("/edit/{other:.*}")
-                .setViewName("forward:/index.html")
+            .setViewName("forward:/index.html")
+
     }
 
 
